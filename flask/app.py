9a7efbb9b23a77224c1946d3a5174f7b4c2e4a8a
JakeACross <jackgw@mail.umkc.edu>
2022-05-05 16:51:22 -0500

    Simple web page

--- flask/app.py
+++ flask/app.py
@@ -5,10 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/makeMdl")
+def makeMdl():
+    return render_template("makeMdl.html")
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/faceRec")
+def faceRec():
+    return render_template("faceRec.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+''' Hold shift and click reload to update css -> https://www.pythonanywhere.com/forums/topic/7425/ '''
